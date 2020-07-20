6eef3fe8c38d881da55dba63c31f5241cd9f2df1
abed-laptop <alavi.khorasani@gmail.com>
2020-07-20 14:50:04 +0430

    expand examples

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, Fragment } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
-import PersianCalendar from 'react_simple_persian_calendar';
+import PersianCalendar from "react_simple_persian_calendar";
 function App() {
   const [selectedTime, setSelecteTime] = useState();
   const onChange = (date) => {
@@ -12,23 +12,40 @@ function App() {
 
   return (
     <Fragment>
-      <div class="jumbotron text-center">
-        <h1 class="display-4 ">React Simple Persian Calendar</h1>
-        <p class="lead">
+      <div className="jumbotron text-center">
+        <h1 className="display-4 ">React Simple Persian Calendar</h1>
+        <p className="lead">
           This is the page in which you can see{" "}
           <a href="https://www.npmjs.com/package/react_simple_persian_calendar">
             react_simple_Persian_calendar
           </a>{" "}
           component example.
         </p>
-        <hr class="my-4" />
-        <div className="container" style={{ height: "250px" }}>
-          <p>Click on the Component below:</p>
-          <PersianCalendar onChange={onChange} />
+        <hr className="my-4" />
+        <div className="container containerStyle">
+          <div className="row rowstyle">
+            <div className="col-sm text-center first-column">Without currentTime props:</div>
+            <div className="col-sm text-center second-column">
+              <PersianCalendar
+                onChange={onChange}
+                // currentTime={new Date("1987,01,22")}
+              />
+            </div>
+          </div>
+          <hr/>
+          <div className="row rowstyle">
+            <div className="col-sm text-center first-column">With currentTime: new Date("1987,01,22"):</div>
+            <div className="col-sm text-center second-column">
+              <PersianCalendar
+                onChange={onChange}
+                currentTime={new Date("1987,01,22")}
+              />
+            </div>
+          </div>
         </div>
       </div>
-      <footer class="footer mt-auto py-3">
-        <div class="container">
+      <footer className="footer mt-auto py-3">
+        <div className="container">
           <div className="row">
             <div className="col">
               {" "}
@@ -39,11 +56,11 @@ function App() {
             </div>
             <div className="col text-center">
               <h1>
-                <a href="https://github.com/abedalavi/react_simple_Persian_calendar">
+                <a href="https://github.com/abedalavi/react_simple_persian_calendar">
                   <i className="fab fa-github"></i>
                 </a>
                 {"    "}
-                <a href="https://www.npmjs.com/package/react_simple_Persian_calendar">
+                <a href="https://www.npmjs.com/package/react_simple_persian_calendar">
                   <i className="fab fa-npm"></i>
                 </a>
               </h1>
